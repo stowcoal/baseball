471a5f9b0f5cc72676f7a0bb604247ff4bb8fceb
Curtis Stochl <stowcoal@nezined.PK5001Z>
2013-08-10 00:29:57 -0500

    Added Pitch class and created a print functionality for all dat.

---
+++ Pitch.java
@@ -0,0 +1,15 @@
+public class Pitch{
+    public String type;
+    public String result;
+    public int speed; // mph
+    public Pitch(String t, Integer s, String r)
+    {
+	type = t;
+	result = r;
+	speed = s;
+    }
+    public void print()
+    {
+	System.out.println(String.format("%-25s%-5d%s", type, speed, result));
+    }
+}
